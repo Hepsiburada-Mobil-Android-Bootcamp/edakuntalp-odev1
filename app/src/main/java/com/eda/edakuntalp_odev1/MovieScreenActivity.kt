7667952b1e80160a5ce3fcbd05dc8fb4365468bc
edakuntalp <eda.kuntalp@ceng.deu.edu.tr>
2021-10-15 00:18:44 +0300

    UI screens before editting

---
+++ app/src/main/java/com/eda/edakuntalp_odev1/MovieScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.eda.edakuntalp_odev1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_screen)
+    }
+}
